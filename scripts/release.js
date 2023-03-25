c43c85944d9625d976f0f741b18cac88b38f7c6c
chenzhongsheng <chenzhongsheng@bytedance.com>
2023-03-25 13:39:31 +0800

    feat: 0.0.7 fix

--- scripts/release.js
+++ scripts/release.js
@@ -5,14 +5,12 @@
  */
 const {exec} = require('./utils');
 
-
 async function main () {
     const version = process.argv[2];
     console.log(`Start build version=${version}`);
     await exec(`npm run build ${version}`);
     console.log(`Build success, publishing...`);
-    await exec(`cd npm`);
-    await exec(`npm publish`);
+    await exec(`cd npm && npm publish`);
     console.log(`Publish success`);
 }
 
--- scripts/utils.js
+++ scripts/utils.js
@@ -73,7 +73,7 @@ function buildPackageJson (extract = {}) {
     const attrs = [
         'name', 'version', 'description', 'main', 'unpkg', 'jsdelivr', 'typings',
         'repository', 'keywords', 'author', 'license', 'bugs', 'homepage',
-        'dependencies', 'module'
+        'dependencies', 'module', 'publishConfig'
     ];
 
     const npmPkg = {};
